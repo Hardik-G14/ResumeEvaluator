ba23859c7db54cc404a6d9e491961671d3acb782
kashish kapoor <kashishkapoor310@kashishs-MacBook-Air.local>
2025-11-07 00:16:46 +0530

    changes

--- src/graph/stategraph.py
+++ src/graph/stategraph.py
@@ -4,18 +4,28 @@ from src.graph.state.graph_state import ResumeState
 from src.graph.nodes.pdf_loader import pdf_loader
 from src.graph.nodes.skills_evaluator import skills_evaluator
 from src.graph.nodes.personal_evaluator import personal_info_extractor
+from src.graph.nodes.project_evaluator import project_evaluator
+from src.graph.nodes.education_evaluator import education_evaluator
+
 def create_graph():
     graph = StateGraph(ResumeState)
 
     graph.add_node("pdf_loader", pdf_loader)
     graph.add_node("skills_evaluator", skills_evaluator)
+    graph.add_node("project_evaluator", project_evaluator)
+
     graph.add_node("personal_info_evaluator", personal_info_extractor)
+    graph.add_node("education_evaluator", education_evaluator)
 
     graph.add_edge(START, "pdf_loader")
     graph.add_edge("pdf_loader", "skills_evaluator")
     graph.add_edge("pdf_loader", "personal_info_evaluator")
+    graph.add_edge("pdf_loader", "project_evaluator")
+    graph.add_edge("pdf_loader", "education_evaluator")
     graph.add_edge("skills_evaluator", END)
     graph.add_edge("personal_info_evaluator", END)
+    graph.add_edge("project_evaluator", END)
+    graph.add_edge("education_evaluator", END)
 
     compile_graph = graph.compile()
 
